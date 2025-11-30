4c2f5201dc7c0e545821a4fe57a14e6ecab8d469
ehmedlifatime <ehmedli8581@icloud.com>
2025-12-01 02:17:04 +0400

    Add files via upload

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,25 @@
-from django.db import models
+from django.db import models # <-- Bu sətir 'models' obyektini təyin edir
 from django.utils import timezone
-import datetime
+import datetime # 'datetime' modulunu import edir
 
+# --- Question modeli ---
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def _str_(self):
+        return self.question_text
+    
     def was_published_recently(self):
         now = timezone.now()
+        # Tutorial 6-ya uyğun: 1 gündən az keçibsə və gələcək deyilsə
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+# --- Choice modeli ---
+class Choice(models.Model): # <-- 'Choice' sinfinin 'C' hərfi böyük olmalıdır
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def _str_(self):
+        return self.choice_text
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from . import views
+
+from . import views # Həmin kataloqdakı views.py faylını import edir
+
+# Bu, reverse lookup üçün ad sahəsidir (namespace)
+app_name = 'polls' 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # /polls/
+    path('', views.IndexView.as_view(), name='index'), 
+
+    # /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), 
+
+    # /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    # /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
